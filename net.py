7ce94af5aac52158e208999ab860d84402818a23
朝闻道 <138562772+future-dream01@users.noreply.github.com>
2024-07-18 16:47:34 +0800

    增加了 pytorch 框架的一些现有模块和函数

--- net.py
+++
@@ -1,9 +0,0 @@
-import torch
-import numpy as np
-import torch.nn as nn       # 神经网络模块
-
-class Lnet(nn.Model):
-    def __init__(self):
-        super(Lnet,self).__init__()
-        self.conv1=nn.
-    
