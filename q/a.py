f77cae0d1cf3c6fa998bbea6637c4a8eb09233d6
朝闻道 <1561404532@qq.com>
2024-11-27 16:35:26 +0800

    1. python笔记增加了语句

--- q/a.py
+++ q/a.py
@@ -1,1 +1,16 @@
-import torch
+from sympy import symbols,Eq,solve
+import math
+
+plist=[320083,304292,285660,263307,243037,218087,199698,158401]
+flist=[]
+lamda=[0.66470534]
+for i in range(0,len(plist)-1):
+    lamda2=symbols('lamda2',positive=True)
+    equation1=Eq(plist[i+1]/plist[i],(lamda[i]/lamda2)*((2.4-0.4*lamda2**2)/(2.4-0.4*lamda[i]**2)))
+    lamda2=solve(equation1,lamda2)[0]
+    f=symbols('f',positive=True)
+    equation2=Eq(4*f*(0.18/0.009),(2.4/2.8)*((1/(lamda[i]**2))-(1/(lamda2**2))+math.log((lamda[i]**2)/(lamda2**2))))
+    out=solve(equation2,f)
+    lamda.append(lamda2)
+    flist.append(out)
+print("摩擦系数列表为：",flist)
--- q/d.py
+++ q/d.py
@@ -1,1 +1,7 @@
-from .g import c
+# from sympy import symbols,Eq,solve
+import math
+
+# f=symbols('f',positive=True)
+# equation2=Eq(4*f*(0.18/0.009),(2.4/2.8)*((1/0.995959**2)-(1/1.059367**2)+math.log(0.995959**2/1.059367**2)))
+# print(solve(equation2,f))
+print(math.log(10))
